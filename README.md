a47ddf3297c25640b5eb265a29ecd69afa947b3e
Luisa Burini <lburini@unicamp.br>
2025-09-05 00:43:54 -0300

    :bug: corrige codigo rastreador solar

--- README.md
+++ README.md
@@ -2,14 +2,40 @@
 
 Instruções de montagem para o trabalho Neosymbiota Lucens tentativa 1
 
+Essa parte do trabalho (do rastreador de luz) foi montado a partir da [ref 1](https://www.usinainfo.com.br/blog/rastreador-solar-com-arduino-um-seguidor-solar-atraves-de-ldr/?srsltid=AfmBOopmhUIaa4iWFgExYoY_OMAmqnunysOihkISRG3wj58gcAvz8ka0).
+O rastreador usa LDR (Light Dependent Resistor, isto é, resistores variáveis com a luz) para detectar a presença de luz em 4 quadrantes divididos por uma altura, de modo que faça sombra nos demais LDRs quando não está alinhado com a fonte de luz.
+Para ajustar a posição do circuito de modo a se alinhar com a fonte de luz, foram usados 2 servo motores MG966R, que são motores servo de rotação CONTÍNUA. Um motor gira entre esquerda e direita na horizontal, e o outro gira para cima e para baixo, na vertical, perpendicular ao outro motor.
+```
+            |   
+     A2     |     A0    
+------------|-------------
+     A3     |     A1
+            |
+```
+
+O algoritmo funciona a partir da diferença entre a luz percebida entre os LDRs (conectados em A0, A1, A2, A3). Quando a luz tem mais intensidade:
+- A direita, o motor horizontal gira no sentido ANTI HORARIO
+- A esquerda, o motor horizontal gira no sentido HORARIO
+- Acima, o motor vertical gira no sentido ANTI HORARIO
+- Abaixo, o motor vertical gira no sentido HORARIO
+
+## *UMA OBSERVAÇÃO IMPORTANTE*
+
+O servo motor utilizado é de rotação contínua, então, apesar das conexões serem idênticas ao do motor servo de passo, o motor de rotação contínua gira 360 graus. Também, a escrita nele não indica quantos graus o motor virará, na verdade, ele usa 
+- 0 para rotação no sentido HORARIO
+- 90 para PARAR
+- 180 para rotação no sentido ANTI HORARIO
+
 ![Diagrama do projeto](./imgs/rastreador.jpg)
+Créditos da imagem para [ref 1](https://www.usinainfo.com.br/blog/rastreador-solar-com-arduino-um-seguidor-solar-atraves-de-ldr/?srsltid=AfmBOopmhUIaa4iWFgExYoY_OMAmqnunysOihkISRG3wj58gcAvz8ka0)
 
 ## Funcionalidades
 
 Integração do Arduino UNO com
 
-* Placa solar: [ref 1](https://www.paraisodosbits.com.br/2016/11/09/usando-o-arduino-com-painel-solar/), [ref 2](https://www.instructables.com/Self-Sufficient-Arduino-Board/), [ref 3](https://docs.arduino.cc/learn/electronics/power-pins/)
-* Bomba aquática submersível: [ref 1](https://www.youtube.com/watch?v=HBGYIuBC3BQ), [ref 2](https://www.youtube.com/watch?v=yZqnqxHI59Q)
+* Rastreador de luz [ref 1](https://www.usinainfo.com.br/blog/rastreador-solar-com-arduino-um-seguidor-solar-atraves-de-ldr/?srsltid=AfmBOopmhUIaa4iWFgExYoY_OMAmqnunysOihkISRG3wj58gcAvz8ka0)
+* Placa solar: [ref 2](https://www.paraisodosbits.com.br/2016/11/09/usando-o-arduino-com-painel-solar/), [ref 3](https://www.instructables.com/Self-Sufficient-Arduino-Board/), [ref 4](https://docs.arduino.cc/learn/electronics/power-pins/)
+* Bomba aquática submersível: [ref 5](https://www.youtube.com/watch?v=HBGYIuBC3BQ), [ref 6](https://www.youtube.com/watch?v=yZqnqxHI59Q)
 
 
 
--- sketch/sketch.ino
+++ sketch/sketch.ino
@@ -1,42 +1,41 @@
-#include <Servo.h>          // Inclui a Biblioteca
+
+#include <Servo.h>          
+
+// Codigo para uso com motor servo de rotacao continua
+int SENTIDO_HORARIO = 0;// 0: velocidade maxima em sentido horario
+int PARADO = 90; // 90: parado
+int SENTIDO_ANTI_HORARIO = 180; // 180: velocidade maxima em sentido anti-horario 
  
 Servo Horizontal;           // Inicia o Servo da Horizontal
 Servo Vertical;             // Inicia o Servo da Vertical
 
+int PINO_HORIZONTAL = 3;
+int PINO_VERTICAL = 5;
+
 int ServoHorizontal = 90;   // Estabelece valor fixo à ServoHorizontal
 int ServoVertical = 90;     // Estabelece valor fixo à ServoVertical
  
-int LimiteServoHorizontalMax = 180;  // Estabele os limites de rotação
-int LimiteServoHorizontalMin = 65;   // Estabele os limites de rotação
- 
-int LimiteServoVerticalMax = 120;    // Estabele os limites de rotação
-int LimiteServoVerticalMin = 15;     // Estabele os limites de rotação
- 
- int LDRDC = A0;             // Inicia LDRDC no pino A0
+int LDRDC = A0;             // Inicia LDRDC no pino A0
 int LDRDB = A1;             // Inicia LDRDB no pino A1
 int LDREC = A2;             // Inicia LDREC no pino A2
 int LDREB = A3;             // Inicia LDREB no pino A3
  
-
-void setup() {
-  // put your setup code here, to run once:
-  Serial.begin(115200);
-  Horizontal.attach(3);     // Inicia servo Horizontal na porta D3
-  Vertical.attach(5);       // Inicia servo Vertical na porta D5
- 
-  Horizontal.write(180);    // Inicia servo Horizontal na posição 180
-  Vertical.write(45);       // Inicia servo Horizontal na posição 45
+void setup()  {
+  Serial.begin(9600);
+  Vertical.attach(PINO_VERTICAL);
+  Vertical.write(PARADO);
+  Horizontal.attach(PINO_HORIZONTAL);
+  Horizontal.write(PARADO);
   delay(3000);              // Aguarda 3 segundos
 }
-
-void loop() {
-  // put your main code here, to run repeatedly:
- int LDC = analogRead(LDRDC);      // Leitura Analógica do LDR Direita Cima
-  int LEC = analogRead(LDREC);      // Leitura Analógica do LDR Esquerda Cima
-  int LDB = analogRead(LDRDB);      // Leitura Analógica do LDR Direita Baixo
-  int LEB = analogRead(LDREB);      // Leitura Analógica do LDR Esquerda Baixo
  
-  int tol = 50;
+void loop() {
+  int LDC = analogRead(LDRDC);      // Leitura Analógica do LDR Direita Cima
+  int LEC = analogRead(LDREC);      // Leitura Analógica do LDR Esquerda Cima
+  int LDB = analogRead(LDRDB);      // Leitura Analógica do LDR Direita PINO_HORIZONTAL
+  int LEB = analogRead(LDREB);      // Leitura Analógica do LDR Esquerda PINO_HORIZONTAL
+ 
+  int tol = 20;
  
   int ValorSup = (LDC + LEC) / 2;   // Média da leitura dos LDR superior
   int ValorInf = (LDB + LEB) / 2;   // Média da leitura dos LDR inferior
@@ -47,43 +46,39 @@ void loop() {
   int DifDirEsq = ValorDir - ValorEsq;      // Diferença entre LED direita e esquerda
  
   /*---------------------------------------------------*/
- 
+  Serial.print("Diff sup inf ");
+  Serial.println(DifSupInf);
   // Realiza a leitura e executa os movimentos referente ao Servo Vertical
   if (-1 * tol > DifSupInf || DifSupInf > tol)  {
     if (ValorSup > ValorInf)  {
-      ServoVertical = ++ServoVertical;
-      if (ServoVertical > LimiteServoVerticalMax)  {
-        ServoVertical = LimiteServoVerticalMax;
-      }
+      ServoVertical = SENTIDO_ANTI_HORARIO;
     }
     else if (ValorSup < ValorInf)  {
-      ServoVertical = --ServoVertical;
-      if (ServoVertical < LimiteServoVerticalMin)  {
-        ServoVertical = LimiteServoVerticalMin;
-      }
+      ServoVertical = SENTIDO_HORARIO;
     }
+    Serial.print("Servo vertical ");
+    Serial.println(ServoVertical);
     Vertical.write(ServoVertical);
+  } else {
+    Vertical.write(PARADO);
   }
  
+  Serial.print("Diff dir esq ");
+  Serial.println(DifDirEsq);
   // Realiza a leitura e executa os movimentos referente ao Servo Horizontal
   if (-1 * tol > DifDirEsq || DifDirEsq > tol) {
     if (ValorDir > ValorEsq)  {
-      ServoHorizontal = --ServoHorizontal;
-      if (ServoHorizontal < LimiteServoHorizontalMin)  {
-        ServoHorizontal = LimiteServoHorizontalMin;
-      }
+      ServoHorizontal = SENTIDO_ANTI_HORARIO;
     }
     else if (ValorDir < ValorEsq)  {
-      ServoHorizontal = ++ServoHorizontal;
-      if (ServoHorizontal > LimiteServoHorizontalMax)  {
-        ServoHorizontal = LimiteServoHorizontalMax;
-      }
+      ServoHorizontal = SENTIDO_HORARIO;
     }
-    else if (ValorDir = ValorEsq)  {
-    }
+    Serial.print("Servo horizontal ");
+    Serial.println(ServoHorizontal);
     Horizontal.write(ServoHorizontal);
+  } else {
+    Horizontal.write(PARADO);
   }
  
-  delay(100);   // Aguarda 0,1 segundo
+  delay(10);   // Aguarda 0,1 segundo
 }
-
