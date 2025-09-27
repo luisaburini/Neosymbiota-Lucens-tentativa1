e508c1a7cdde6d362e8d9cb721746bff35ca983c
Luisa Burini <lburini@unicamp.br>
2025-09-27 14:02:33 -0300

    :bug: conserta para nema 34

--- sketch/sketch.ino
+++ sketch/sketch.ino
@@ -4,6 +4,8 @@ const int PULSO = 5;
 int LDRDC = A0;             // Inicia LDRDC no pino A0
 int LDRDB = A1;             // Inicia LDRDB no pino A1
  
+int times = 100;
+
 void setup()  {
   Serial.begin(9600);
   pinMode(DIRECAO, OUTPUT);
@@ -15,7 +17,7 @@ void loop() {
   int LDC = analogRead(LDRDC);      // Leitura Analógica do LDR Direita Cima
   int LDB = analogRead(LDRDB);      // Leitura Analógica do LDR Direita PINO_HORIZONTAL
 
-  int tol = 50;
+  int tol = 100;
  
   int ValorSup = LDC;   // Média da leitura dos LDR superior
   int ValorInf = LDB;   // Média da leitura dos LDR inferior
@@ -23,22 +25,25 @@ void loop() {
   int DifSupInf = ValorSup - ValorInf;      // Diferença entre LED superior e inferior
  
   /*---------------------------------------------------*/
-  Serial.print("Diff sup inf ");
-  Serial.println(DifSupInf);
   // Realiza a leitura e executa os movimentos referente ao motor de passo vertical
   if (-1 * tol > DifSupInf || DifSupInf > tol)  {
+  Serial.print("Diff sup inf ");
+  Serial.println(DifSupInf);
     Serial.print("Servo vertical ");
     if (ValorSup > ValorInf)  {
       Serial.println("LOW");
       digitalWrite(DIRECAO, LOW);
     }
-    else if (ValorSup < ValorInf)  {
+    else { // (ValorSup < ValorInf) 
       Serial.println("HIGH");
       digitalWrite(DIRECAO, HIGH);
     }
+    for(int i = 0; i < 1000; i++) {
+      digitalWrite(PULSO, HIGH);
+      delayMicroseconds(300);
+      digitalWrite(PULSO, LOW);
+      delayMicroseconds(300);
+    }
   }
-  digitalWrite(PULSO, LOW);
-  digitalWrite(PULSO, HIGH); 
- 
-  delay(10);   // Aguarda 0,1 segundo
+  delay(110);   
 }
